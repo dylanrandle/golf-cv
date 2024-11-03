bd6dd6511e738a188e41dd6f4d6cc5337a219d3a
Dylan Randle <dylanrandle@rogers.com>
2024-11-03 13:39:02 -0500

    feat: add keypoints to videos

--- src/predict_poses.py
+++ src/predict_poses.py
@@ -7,20 +7,39 @@ from mmpose.apis import MMPoseInferencer
 logger = logging.getLogger(__name__)
 
 
+MODES = {"2d", "3d"}
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--video-path", type=str, required=True)
     parser.add_argument("-o", "--output-path", type=str, required=True)
+    parser.add_argument("-m", "--mode", type=str, choices=MODES)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    inferencer = MMPoseInferencer(pose3d="human3d")
+    inference_mode = args.mode.strip().lower()
+
+    if inference_mode == "3d":
+        inferencer = MMPoseInferencer(pose3d="human3d")
+    elif inference_mode == "2d":
+        inferencer = MMPoseInferencer(pose2d="human")
+    else:
+        raise ValueError(
+            f"Unrecognized inference mode: {inference_mode} (valid types are: {MODES})"
+        )
 
     results = inferencer(
-        args.video_path, show=False, draw_bbox=True, out_dir=args.output_path
+        args.video_path,
+        show=False,
+        draw_bbox=True,
+        out_dir=args.output_path,
+        radius=6,
+        thickness=2,
+        skeleton_style="openpose",
     )
     results = [result for result in results]
 
